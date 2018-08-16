dd3489f84c3775b585732d681abfebff0e429d94
Shubham Singh <shubhamsingh@Shubham.Singh>
2018-08-16 13:56:14 +0530

    First commit project

--- src/main/java/com/oodles/controller/HelloForm.java
+++ src/main/java/com/oodles/controller/HelloForm.java
@@ -1,31 +1,46 @@
 package com.oodles.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloForm {
-	
-	@PostMapping("/detail")
-	public String getting(@RequestParam("name")String  name ,@RequestParam("message")String message ,Model model)
-	{ 
-		model.addAttribute("name",name); 
-		model.addAttribute("message",message); 
-		return "hv";	
+
+	Logger logger = LoggerFactory.getLogger(HelloForm.class);//for Logging 
+
+	@PostMapping("/detail") //Mapping the data after submission 
+	public String getting(@RequestParam("name") String name, @RequestParam("message") String message, Model model) {
+		logger.debug("Debugging method");
+		logger.info("Info method");
+		logger.warn("Warning method");
+		logger.error("Error method");
+
+		model.addAttribute("name", name);
+		model.addAttribute("message", message);
+		logger.debug("End Debugging method");
+		logger.info("End Info method");
+		logger.warn("End Warning method");
+		logger.error("End Error method");
+		return "hv";
 	}
-	@GetMapping("/")
-	public String accept()
+
+	@GetMapping("/")     //Getting the form
+	public String accept() {
+		logger.debug("Debugging method");
+		logger.info("Info method");
+		logger.warn("Warning method");
+		logger.error("Error method");
+
+		return "hz";
+	}
+
 	{
-		System.out.println("one started");
-		System.out.println("one finished");
-		return "hz";	
+
 	}
-	{
-		
-	}
-	
+
 }
