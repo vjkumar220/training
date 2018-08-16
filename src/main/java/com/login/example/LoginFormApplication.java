d2c33b7e58db5963016fd7c5c24d103723638d9f
shivam d sahu <shivam@shivam.d>
2018-08-16 13:56:47 +0530

    first commit loginform

---
+++ src/main/java/com/login/example/LoginFormApplication.java
@@ -0,0 +1,12 @@
+package com.login.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginFormApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginFormApplication.class, args);
+	}
+}
