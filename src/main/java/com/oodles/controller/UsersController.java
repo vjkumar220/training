7bca9a32d1f60555aa8b4df0196943a73f5a15b8
Shubham Singh <shubhamsingh@Shubham.Singh>
2018-08-22 15:46:48 +0530

    Crud implemention with create,update and delete

--- src/main/java/com/oodles/controller/UsersController.java
+++ src/main/java/com/oodles/controller/UsersController.java
@@ -1,14 +1,18 @@
 package com.oodles.controller;
 
 import java.util.Map;
+import java.util.NoSuchElementException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.oodles.domain.User;
 import com.oodles.example.ResourceNotFound;
 import com.oodles.example.UserNotFoundException;
 import com.oodles.services.UsersService;
@@ -18,9 +22,9 @@ import com.oodles.services.UsersService;
 public class UsersController {
 	@Autowired
 	private UsersService userService;	
-	
+	//Create 
 @RequestMapping(method = RequestMethod.GET, value = "/rest/createuser/{name}")
-	
+	@ResponseBody
 	public Map createUser(@PathVariable String name)  {
 		 Map result=null;
 		
@@ -31,6 +35,47 @@ public class UsersController {
 		catch(UserNotFoundException exception){
 			return ResourceNotFound.generateResponse(HttpStatus.INTERNAL_SERVER_ERROR, true, "error", null, result);
 		}
-}	
+}
+
+//Update
+@RequestMapping(method = RequestMethod.GET, value = "/rest/updateuser/{id}/{name}")
+@ResponseBody
+public Map updateUser(@PathVariable String id ,@PathVariable String name)
+{
+User user=null;
+try {
+	user =userService.updateUser(id,name);
+	return ResourceNotFound.generateResponse(HttpStatus.OK, false, "success", null, user);
+}
+catch(UserNotFoundException exception){
+	return ResourceNotFound.generateResponse(HttpStatus.INTERNAL_SERVER_ERROR, true, "error", null, user);
+}
+catch(NoSuchElementException excep){
+	return ResourceNotFound.generateResponse(HttpStatus.INTERNAL_SERVER_ERROR, true, "error", null, user);
+}
+}
+// Delete
+
+
+
+//@DeleteMapping("/deleteusers/{id}")
+@RequestMapping(method = RequestMethod.GET, value = "/deleteusers/{id}")
+@ResponseBody
+public Map deleteUser(@PathVariable String id)
+{
+	User result=null;
+		
+		try{
+		 result=userService.deleteUser(id);
+			return ResourceNotFound.generateResponse(HttpStatus.OK, false, "success", null, result);
+		}
+		catch(UserNotFoundException exception){
+			return ResourceNotFound.generateResponse(HttpStatus.INTERNAL_SERVER_ERROR, true, "error", null, result);
+		}
+		catch(NoSuchElementException excep){
+			return ResourceNotFound.generateResponse(HttpStatus.INTERNAL_SERVER_ERROR, true, "error", null, result);
+		}
+}
+
 }
 
--- src/main/java/com/oodles/services/UsersService.java
+++ src/main/java/com/oodles/services/UsersService.java
@@ -1,7 +1,10 @@
 package com.oodles.services;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -16,7 +19,7 @@ public class UsersService {
 	public static final Logger logger = LoggerFactory.getLogger(UsersService.class);
 	@Autowired
 	private UsersRepository usersRepository;
-
+//create
 	public Map<String, Object> createUser(String name) {
 		Map<String, Object> result = new HashMap<String, Object>();
 		User user = usersRepository.findByName(name);
@@ -31,4 +34,49 @@ public class UsersService {
 		return result;
 	}
 	
+	//Update
+	
+	public User updateUser(String id,String name)
+	{
+		Optional<User> value=usersRepository.findById(Long.parseLong(id));
+		 User user = value.get();
+		 if(value.isPresent() && (!user.getName().equalsIgnoreCase(name)))
+		 
+	
+		{
+			 User newUsers=new User();
+			 newUsers.setId(Long.parseLong(id));
+			newUsers.setName(name);
+			usersRepository.save(newUsers);
+		}		
+		return user;
+	}
+	
+	//Delete 
+	public  User deleteUser(String id) {
+		
+		Optional<User> user = usersRepository.findById(Long.parseLong(id));
+		 User result = user.get();
+		if(user.isPresent()&& (!result.getId().equals(id)))
+		{
+			User newUser=new User();
+			
+				
+			usersRepository.deleteById(Long.parseLong(id));
+			
+			
+		}
+		return result;
+	}
 }
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
